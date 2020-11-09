887b94e9910dc3ccdd12a9d57955b634565ebe51
rerades <rerades@siete3.com>
2020-11-09 18:42:36 +0100

    refactoring

--- 01-funciones-puras/VendingMachine.js
+++ 01-funciones-puras/VendingMachine.js
@@ -3,7 +3,7 @@
 // --------------
 
 /**
- * @desc COn las funciones anteriormente creadas vamos a implementar una máquina
+ * @desc Con las funciones anteriormente creadas vamos a implementar una máquina
  * expendedora de forma que le pasemos un array de monedas unas coordenadas del producto
  * que queremos y nos devolverá el producto seleccionado junto a la vuelta.
  *
@@ -16,7 +16,7 @@
  * @nota Por simplicidad entendemos que el array de productos lo conoce la máquina y que
  * no necesita acceder a ningún componente externo para recuperar la información
  *
- * @example buyProduct([0.10,0.25,1,2],["A",2]) // --> {product:'Ritz', change:'0.35' }
+ * @example VendingMachine.buyProduct([0.10,0.25,1,2],["A",2]) // --> {product:'Ritz', change:'0.35' }
  */
 
 
@@ -30,34 +30,5 @@ VendingMachine = {
   products
 }
 
-VendingMachine.buyProduct = (coins , coordinates) => {
-  // insertar monedas
-  const balance = coins.reduce((acc,cur) => acc.add(cur), credit(0)) // ?
-  // seleccionar producto
-  const selectedProduct = product(products)(coordinates[0])(coordinates[1]) // ?
-  // comprobar si hay error
-  if (selectedProduct.error) {
-    return {...selectedProduct , change:balance.balance}; // ?
-  }
-  // ver si tengo crédito suficiente para comprar el producto
-  const hasCredit = checkCredit(selectedProduct.product).gte(balance.balance) // ?
-  if (!hasCredit) {
-    return {
-      error: 'no tiene suficiente dinero para comprar el producto',
-      change: balance.balance
-    }
-  }
-  // calcular el cambio
-  const givenChange = change(balance.balance).subtract(selectedProduct.product.price) // ?
-  // retornamos un objeto con el producto y el cambio
-  return {
-    product: selectedProduct.product.name,
-    change:givenChange.credit
-  }
+// VendingMachine.buyProduct = (coins , coordinates) => ...
 
-}
-
-VendingMachine.buyProduct([0.10,0.25,1,0.2],["A",7])  // ?
-
-
-module.exports = {products, product, checkCredit, change, VendingMachine}
