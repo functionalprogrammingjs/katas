6f7e12f18dafbba579cc78431ffacebf05747e68
rerades <rerades@siete3.com>
2020-11-09 19:02:37 +0100

    adding info

--- 01-funciones-puras/credit.js
+++ 01-funciones-puras/credit.js
@@ -4,7 +4,7 @@
 
 /**
  * @desc Implementar una función credit a la que se le puedan pasar cantidades y pueda sumarlas
- * @example const totalBalance = credit(50).add(10).add(20)
+ * @example const totalBalance = credit(50).add(10).add(20) // {balance:80, add:function}
  * @Hindley-Milner  credit :: number -> a
  * @ADT semigroup (with an empty will be a monoid)
  */
