9e620580d38129d85f75125bd8bc8e432100dc8d
rerades <rerades@siete3.com>
2020-11-12 06:50:49 +0100

    wip

--- 01-funciones-puras/credit.js
+++ 01-funciones-puras/credit.js
@@ -8,9 +8,21 @@
  * @Hindley-Milner  credit :: number -> a
  * @ADT semigroup (with an empty will be a monoid)
  */
-const credit = balance => ({
+const _credit = balance => ({
   balance,
   add: amount =>  credit(balance + amount),
 })
+
+
+
+/**
+ * Vamos a hacer nuestras funciones genéricas, que no dependan de una estructura
+ * rígida que no nos permita poder extender nuestras funciones o reutilizarlas
+ * */
+
+// FIXME : la función add podríamos sacarla fuera a una función independiente suma
+const add = a => b => a + b;
+
+
 
 module.exports = {credit};
